9d07d1c7ae0d4a05867bac6564d48a010c4fefd6
Mepy <Minpows@outlook.com>
2024-05-28 23:45:24 +0800

    try to NbE thunk/force, but no progress now

---
+++ lib/syntax.ml
@@ -0,0 +1,84 @@
+(* we do namelessly *)
+type expr = 
+  | Index of int (* DeBruijn index *)
+  | Level of int (* DeBruijn level, NOT use when construct AST *)
+  | Type of int (* universe i *)
+  | Let of expr * (* BINDS *) expr (* let _0 = expr in expr *)
+
+  | Pi of typ * (* BINDS *) typ (* Pi A x.B *)
+  | Lam of (* BINDS *) expr (* lam A, x.V *)
+  (* We write let f:Pi T x.A = lam x.M, NO param type *)
+  | App of expr * expr (* f x *)
+
+  | Sigma of expr * (* BINDS *) expr (* Sigma T x.A *)
+  | Pair of expr * expr (* (x, y) : Sigma(x:T) A *)
+  | Fst of expr (* e.1 *)
+  | Snd of expr (* e.2 *)
+
+  | Void    (* False, \{  \}  *)
+  | Exfalso of expr * expr (* Exfalso(falsehood, T) : T *)
+
+  | Unit    (* True,  \{ * \} *)
+  | Trivial (* () : Unit *)
+  | Singleton of expr * (* BINDS *) expr *  expr  (* Singleton(e, x.A, a:A[*/x])*)
+
+  (* Bool might be simpler than Sum, thanks to Sigma types *)
+  | Bool
+  | True
+  | False
+  (* if V then W1 else W2 as A end, W1:A, W2:A *)
+  | If of expr * expr * expr * typ
+
+  | Id of expr * expr * expr (* Id(A, a, a') *)
+  | Refl of expr (* Refl(a) : a=a *)
+  | J of expr * (* BINDS 3 *) expr * (* BINDS *) expr (* J(path:a=a', a.a'.A.t, x.refl) *)
+
+  | Underline of typ (* U A, underline set/forgetful function U *)
+  | Thunk of comp (* thunk M *)
+  (* computation as followed: *)
+  | Free of typ (* F A *)
+  | Force of expr (* force(V) *)
+  | Return of expr (* return V *)
+  | Compose of comp * typ * (* BINDS *) comp (* Compose(M, A, x.N) *)
+  (* return V to x:A. M = M[V/x] *)
+  | CPi of typ * (* BINDS *) comp (* Pi(A, x.M) *)
+  | CLam of typ * (* BINDS *) comp (* CLam(A, x.M) *)
+  | CApp of comp * expr (* M V *)
+  | Get of expr (* Get(loc) *)
+  | Put of expr * expr (* Put(loc, val) *)
+
+and typ = expr (* value type *)
+and comp = expr (* comp term *)
+and ctyp = comp (* comp type *)
+[@@deriving show]
+
+
+(** todo: maybe, we need No levelize if we have no W-induction, just remain here *)
+(** size is the length of env *)
+let rec levelize (size:int) (depth:int) (expr:expr) : expr = 
+  (* when BINDS, depth+=1 *)
+  match expr with
+  | Index i -> if i < depth then Index i else Level (size-(depth-i)-1)
+  | Level l -> Level l
+  | Type universe -> Type universe
+  | Let (e1, e2) -> Let(levelize size depth e1, levelize size (depth+1) e2)
+  | Pi (e1, e2) -> Pi(levelize size depth e1, levelize size (depth+1) e2)
+  | Lam e -> Lam (levelize size (depth+1) e)
+  | App (e1, e2) -> App(levelize size depth e1, levelize size depth e2)
+  | Sigma (e1, e2) -> Sigma(levelize size depth e1, levelize size (depth+1) e2)
+  | Pair (e1, e2) -> Pair (levelize size depth e1, levelize size depth e2)
+  | Fst e -> Fst (levelize size depth e)
+  | Snd e -> Snd (levelize size depth e)
+  | Void -> Void
+  | Exfalso (e1, e2) -> Exfalso (levelize size depth e1, levelize size depth e2)
+  | Unit -> Unit
+  | Trivial -> Trivial
+  | Singleton (e1, e2, e3) -> Singleton (levelize size depth e1, levelize size (depth+1) e2, levelize size depth e3)
+  | Bool -> Bool
+  | True -> True
+  | False -> False
+  | If (e1, e2, e3, e4) -> If (levelize size depth e1, levelize size depth e2, levelize size depth e3, levelize size depth e4)
+  | Id (e1, e2, e3) -> Id (levelize size depth e1, levelize size depth e2, levelize size depth e3)
+  | Refl e -> Refl (levelize size depth e)
+  | J (e1, e2, e3) -> J (levelize size depth e1, levelize size (depth+3) e2, levelize size (depth+1) e3)
+  | _ -> failwith "todo"
---
+++ lib/work.ml
@@ -0,0 +1,246 @@
+[@@@warning "-27"]
+
+module S = Syntax
+
+module Nbe = struct
+
+type env = value list
+and clos = | Clos of {expr:S.expr; env:env}
+and clos2 = | Clos2 of {expr:S.expr; env:env}
+and clos3 = | Clos3 of {expr:S.expr; env:env}
+
+and value =
+  | Neutral of {typ:value; term:neutral}
+  | Type of int (* universe *)
+  | Lam of clos
+  | Pi of value * clos
+  | Sigma of value * clos
+  | Pair of value * value
+  | Void
+  | Unit
+  | Trivial
+  | Bool
+  | True
+  | False
+  | Refl of value
+  | Id of value * value * value
+
+  | Underline of value
+  | Free of value
+
+  | Return of value 
+  | CLam of clos
+  | CPi of value * clos
+  | Get of value 
+  | Put of value
+and neutral = 
+  | Level of int (* DeBruijn level *)
+  | App of neutral * normal
+  | Fst of neutral
+  | Snd of neutral
+  | Exfalso of neutral * value
+  | Singleton of neutral * clos * value
+  | If of neutral * value * value * value
+  | J of neutral * clos3 * clos * value * value * value
+
+  | Thunk of neutral
+  | Force of neutral
+  | Compose of neutral * value * clos 
+  | CApp of neutral * normal
+  
+and normal = | Normal of {typ:value; value:value}
+[@@deriving show]
+
+let mk_var typ level = Neutral {typ; term=Level level}
+
+module S = Syntax
+let rec eval (env:env) (expr:S.expr) : value = 
+  match expr with
+  | S.Index i -> List.nth env i
+  | S.Level l -> List.nth env (List.length env -l-1)
+  | S.Type universe -> Type universe
+  | S.Let (def, body) -> 
+    let env' = (eval env def) :: env in
+    eval env' body
+  | S.Pi (src, dst) -> Pi (eval env src, Clos {expr=dst; env})
+  | S.Lam (body) -> Lam (Clos {expr=body; env})
+  | S.App (func, arg) -> 
+    let func = eval env func in
+    let arg = eval env arg in
+    eval_app func arg
+  | S.Sigma (e1, e2) -> Sigma (eval env e1, Clos {expr=e2; env})
+  | S.Pair (e1, e2) -> Pair (eval env e1, eval env e2)
+  | S.Fst expr -> eval_fst (eval env expr)
+  | S.Snd expr -> eval_snd (eval env expr)
+  | S.Void -> Void
+  | S.Exfalso (falsehood, typ) -> eval_exfalso (eval env falsehood) (eval env typ)
+  | S.Unit -> Unit
+  | S.Trivial -> Trivial
+  | S.Singleton(e, a_typ, a) -> eval_singleton (eval env e) (Clos{expr=a_typ; env}) (eval env a)
+  | S.Bool -> Bool
+  | S.True -> True
+  | S.False -> False
+  | S.If(bb, tt, ff, typ) -> 
+    eval_if env bb tt ff typ 
+  | S.Id (typ, left, right) -> Id (eval env typ, eval env left, eval env right) 
+  | S.Refl expr -> Refl (eval env expr)
+  | S.J (path, mot, refl) ->
+    eval_j (Clos3 {expr=mot; env}) (Clos {expr=refl; env}) (eval env path)
+  | S.Underline (typ) -> Underline(eval env typ)
+  | S.Thunk (comp) -> failwith "todo with nf/ne of thunk/force"
+and eval_app (func:value) (arg:value) =
+  match func with
+  | Lam clos -> eval_clos clos arg
+  | Neutral {typ; term} -> (
+    match typ with
+    | Pi (src, dst) -> 
+      let dst = eval_clos dst arg in
+      Neutral {typ=dst; term=App(term, Normal {typ=src; value=arg})}
+    | _ -> failwith "app not_a_pi_type_func _"
+  )
+  | _ -> failwith "app not_a_func _"
+and eval_clos (clos:clos) (arg:value) : value =
+  match clos with
+  | Clos {expr; env} -> 
+    let env' = arg::env in (* add arg into env here *)
+    eval env' expr
+  (* | ConstClos value -> value *)
+and eval_clos2 (Clos2{expr; env}:clos2) (arg1:value) (arg2:value) : value =
+  let env' = arg2::arg1::env in
+  eval env' expr
+and eval_clos3 (Clos3{expr; env}:clos3) (arg1:value) (arg2:value) (arg3:value) : value =
+  let env' = arg3::arg2::arg1::env in
+  eval env' expr 
+
+and eval_fst (pair:value) : value =
+  match pair with
+  | Pair (fst, _) -> fst
+  | Neutral {typ=Sigma(fst_typ, _); term=neutral} ->
+    Neutral {typ=fst_typ; term=Fst neutral}
+  | _ -> failwith "fst not_a_pair"
+and eval_snd (pair:value) : value =
+  match pair with
+  | Pair (_, snd) -> snd
+  | Neutral {typ=Sigma(_, snd_typ_clos); term=neutral} ->
+    let fst = eval_fst pair in
+    let snd_typ = eval_clos snd_typ_clos fst in
+    Neutral {typ=snd_typ; term=Snd neutral}
+  | _ -> failwith "snd not_a_pair"
+and eval_exfalso (falsehood:value) (typ:value) : value =
+  match falsehood with
+  | Neutral {typ=Void; term=neutral} ->
+    Neutral {typ; term=Exfalso(neutral, typ)}
+  | _ -> failwith ("exfalso " ^ (show_value falsehood) ^ " not_a_void")
+and eval_singleton (e:value) (a_typ:clos) (a:value) : value = 
+  match e with
+  | Trivial -> failwith "todo"
+  | Neutral {typ=Unit; term=neutral} -> 
+    let typ = eval_clos a_typ a in
+    Neutral {typ; term=Singleton(neutral, a_typ, a) }
+  | _ -> failwith ("singleton " ^ (show_value a) ^ " not_a_unit")
+and eval_if (env:env) (bb:S.expr) (tt:S.expr) (ff:S.expr) (typ:S.typ) : value =
+  match eval env bb with
+  | True -> eval env tt
+  | False -> eval env ff
+  | Neutral {typ=Bool; term=neutral} -> 
+    Neutral {typ=eval env typ; term=If (neutral, eval env tt, eval env ff, eval env typ) }
+  | _ -> failwith "if not_a_bool"
+and eval_j (mot:clos3) (refl:clos) (path:value) : value =
+  match path with
+  | Refl value -> eval_clos refl value
+  | Neutral {typ; term} -> (
+    match typ with
+    | Id (typ, left, right) -> Neutral 
+      { typ =eval_clos3 mot left right path
+      ; term=J(term, mot, refl, typ, left, right) }
+    | _ -> failwith "j not_an_id_type"
+  )
+  | _ -> failwith "j not_a_path"
+
+let rec quote (size:int) (Normal{typ; value}:normal): S.expr =
+  match typ, value with
+  | Type _, typ -> quote_typ size typ
+  | Pi(src, dst), func -> 
+    let arg = mk_var src size in
+    let normal = Normal {typ=eval_clos dst arg; value=eval_app func arg} in
+    S.Lam (quote (size+1) normal)
+  | Sigma(fst_typ, snd_typ_clos), pair ->
+    let fst = eval_fst pair in
+    let snd_typ = eval_clos snd_typ_clos fst in
+    let snd = eval_snd pair in
+    S.Pair ( quote size (Normal{typ=fst_typ; value=fst})
+           , quote size (Normal{typ=snd_typ; value=snd}) )
+  (* todo : Unit, Trivial -> S.Trivial ??? *)
+  | _, Trivial -> S.Trivial
+  | _, True -> S.True
+  | _, False -> S.False
+  | Id(a_typ, _, _), Refl(a) -> S.Refl(quote size (Normal{typ=a_typ; value=a}))
+  | _, Neutral {term=neutral; _} -> quote_ne size neutral
+  | _ -> 
+    print_endline ((show_value value) ^ " : " ^(show_value typ));
+    failwith "???unknown"
+
+and quote_typ (size:int) (typ:value) : S.expr = 
+  match typ with
+  | Type universe -> S.Type universe
+  | Pi (src, dst) -> 
+    let var = mk_var src size in
+    S.Pi(quote_typ size src, quote_typ (size+1) (eval_clos dst var))
+  | Sigma (fst, snd) ->
+    let var = mk_var fst size in
+    S.Sigma(quote_typ size fst, quote_typ (size+1) (eval_clos snd var))
+  | Void -> S.Void
+  | Unit -> S.Unit
+  | Bool -> S.Bool
+  | Id (typ, left, right) ->
+    S.Id( quote_typ size typ
+        , quote size (Normal{typ; value=left})
+        , quote size (Normal{typ; value=right}) )
+  | Neutral {term; _} -> quote_ne size term
+  | _ -> failwith "quote_typ not_a_type"
+and quote_ne (size:int) (neutral:neutral) : S.expr =
+  match neutral with
+  | Level level -> S.Index (size-(level+1)) (* index = size-(level+1) *)
+  | App(neutral, arg) -> S.App (quote_ne size neutral, quote size arg)
+  | Fst(neutral) -> S.Fst(quote_ne size neutral)
+  | Snd(neutral) -> S.Snd(quote_ne size neutral)
+  | Exfalso(neutral, typ) -> S.Exfalso(quote_ne size neutral, quote_typ size typ)
+  | Singleton(neutral, a_typ, a) -> 
+    let var = mk_var Unit size in 
+    let a_typ' = quote_typ (size+1) (eval_clos a_typ var) in
+    let a' = quote size (Normal{typ=eval_clos a_typ Trivial; value=a}) in
+    S.Singleton(quote_ne size neutral, a_typ', a')
+  | If(bb, tt, ff, typ) -> 
+    let typ' = quote_typ size typ in 
+    S.If(quote_ne size bb, typ',
+    quote size (Normal{typ; value=tt}), 
+    quote size (Normal{typ; value=ff}))
+  | J (path, mot, refl, typ, left, right) -> 
+    let path' = quote_ne size path in
+    let mot_var1 = mk_var typ size in
+    let mot_var2 = mk_var typ (size+1) in 
+    let mot_var3 = mk_var (Id (typ, left, right)) (size+2) in
+    let mot' = quote_typ (size+3) 
+      (eval_clos3 mot mot_var1 mot_var2 mot_var3) in
+    let refl_var = mk_var typ size in
+    let refl' = quote (size+1) 
+      (Normal { typ=eval_clos3 mot refl_var refl_var (Refl refl_var)
+              ; value=eval_clos refl refl_var}) in
+    S.J (path', mot', refl')
+  | _ -> failwith "todo"
+
+let rec initial_env (env:S.typ list) : env * int =
+  match env with
+  | [] -> ([], 0)
+  | typ::env ->
+    let (env', level) = initial_env env in
+    let value = Neutral {typ=eval env' typ; term=Level level} in 
+    (value::env', level + 1)
+
+let normalize (env:S.typ list) (expr:S.expr) (typ:S.typ) : S.expr = 
+  let (env', size) = initial_env env in
+  let typ = eval env' typ in
+  let value = eval env' expr in
+  let normal = Normal {typ; value} in
+  quote size normal
+end
